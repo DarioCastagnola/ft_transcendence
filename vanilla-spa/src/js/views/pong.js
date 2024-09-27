b70d7b070efebc0bce1757b6cc692e99002b1da9
Dario Castagnola <dcastagn@e4r11p1.42roma.it>
2024-09-27 12:06:01 +0200

    Revert "pong 27/09/2024"
    This reverts commit bea80c00c72f7d5831f56a487032c24ffcf72abc.

--- vanilla-spa/src/js/views/pong.js
+++ vanilla-spa/src/js/views/pong.js
@@ -6,31 +6,11 @@ export default function pong() {
     setTimeout(() => {
 		const canvas = document.getElementById("myCanvas");
 		const ctx = canvas.getContext("2d");
-
-		class Player {
-			constructor(x,y) {
-				ctx.fillStyle = "white";
-				ctx.fillRect(x, y, 10, 65);
-			} 
-		}
-
-		class Ball {
-			constructor() {
-				ctx.beginPath();  // Start a new path
-				ctx.arc(200, 200, 50, 0, 2 * Math.PI);  // Define the circle
-				ctx.fillStyle = "blue";  // Set the fill color
-				ctx.fill();  // Fill the circle with the current color
-				ctx.strokeStyle = "white";  // Set the border color
-				ctx.stroke();  // Draw the circle border
-			}
-		}
-
 		ctx.fillStyle = "black";
 		ctx.fillRect(0, 0, 1200, 650);
 		ctx.fillStyle = "white";
-		const Ball1 = new Ball();
-		const Player1 = new Player(30, 280);
-		const Player2 = new Player(1165, 280);
+		ctx.fillRect(30, 280, 10, 65);
+		ctx.fillRect(1165, 280, 10, 65);
     }, 0);
     return html;
 }
