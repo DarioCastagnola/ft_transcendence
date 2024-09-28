1e923a55c21965ee57d4492f7b0349c944a3e934
Dario Castagnola <dcastagn@e4r9p1.42roma.it>
2024-09-28 16:08:45 +0200

    pong structure

--- frontend/src/js/views/pong.js
+++ frontend/src/js/views/pong.js
@@ -6,31 +6,59 @@ export default function pong() {
     setTimeout(() => {
 		const canvas = document.getElementById("myCanvas");
 		const ctx = canvas.getContext("2d");
+		const paddle_distance_from_border = 30;
+		const paddle_height = 65;
+
+		class Paddle {
+			constructor(user) {
+				var height = 65;
+				var width = 10;
+				if (user == "user1")
+				{
+					var x = paddle_distance_from_border;
+					var y = canvas.height / 2 - 50;
+				}
+				else if (user == "user2")
+				{
+					var x = canvas.width - paddle_distance_from_border - width;
+					var y = canvas.height / 2 - 50;
+				}
+				ctx.fillStyle = "white";
+				ctx.fillRect(x, y, width, height);
+			}
+		}
 
 		class Player {
-			constructor(x,y) {
-				ctx.fillStyle = "white";
-				ctx.fillRect(x, y, 10, 65);
+			constructor(user) {
+				var paddle = new Paddle(user);
 			} 
 		}
 
 		class Ball {
-			constructor() {
-				ctx.beginPath();  // Start a new path
-				ctx.arc(200, 200, 50, 0, 2 * Math.PI);  // Define the circle
-				ctx.fillStyle = "blue";  // Set the fill color
-				ctx.fill();  // Fill the circle with the current color
-				ctx.strokeStyle = "white";  // Set the border color
-				ctx.stroke();  // Draw the circle border
+			constructor(x, y) {
+				var height = 10;
+				var width = 10;
+				ctx.fillStyle = "white";  // Set the fill color
+				ctx.fillRect(x, y, width, height);
 			}
 		}
 
-		ctx.fillStyle = "black";
-		ctx.fillRect(0, 0, 1200, 650);
+		class Board {
+			constructor(x, y, width, height) {
+				ctx.fillStyle = "Black";
+				ctx.fillRect(0, 0, width, height)
+			}
+		}
+		const board = new Board(canvas.width / 2 ,canvas.height / 2, canvas.width, canvas.height);
 		ctx.fillStyle = "white";
-		const Ball1 = new Ball();
-		const Player1 = new Player(30, 280);
-		const Player2 = new Player(1165, 280);
+		const ball = new Ball(canvas.width / 2 ,canvas.height / 2);
+		const player1 = new Player("user1");
+		const player2 = new Player("user2");
+		// const player1 = new Player(paddle_distance_from_border, 280, 10, 65);
+		// const player2 = new Player(canvas.width - paddle_distance_from_border - 10, 280, 10, 65);
+
+		ctx.font = "50px Arial";
+		ctx.fillText("Pong", 550, 80);
     }, 0);
     return html;
 }
