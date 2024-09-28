bd651031325e54e2ab21e494fad431906f942ca5
Dario Castagnola <dcastagn@e4r9p1.42roma.it>
2024-09-28 16:37:06 +0200

    size of canvas

--- frontend/src/js/views/pong.js
+++ frontend/src/js/views/pong.js
@@ -1,16 +1,18 @@
 export default function pong() {
     const html = `
-    <canvas id="myCanvas" justify-content="center" width="1200" height="650" style="border:3px solid #ffffff;">
-	</canvas>
+	<div class="row justify-content-center">
+		<div class="col-12 col-md-8">
+    		<canvas class ="w-100" id="myCanvas" style="border:3px solid #ffffff;"></canvas>
+		</div>
+	</div>
     `;
     setTimeout(() => {
 		const canvas = document.getElementById("myCanvas");
 		const ctx = canvas.getContext("2d");
-		const paddle_distance_from_border = 30;
-		const paddle_height = 65;
-
+	
 		class Paddle {
 			constructor(user) {
+				const paddle_distance_from_border = 30;
 				var height = 65;
 				var width = 10;
 				if (user == "user1")
@@ -42,11 +44,23 @@ export default function pong() {
 				ctx.fillRect(x, y, width, height);
 			}
 		}
-
+		
 		class Board {
 			constructor(x, y, width, height) {
-				ctx.fillStyle = "Black";
-				ctx.fillRect(0, 0, width, height)
+				this.resizeCanvas();
+				window.addEventListener('resize', this.resizeCanvas);
+				// ctx.fillStyle = "Black";
+				// ctx.fillRect(0, 0, width, height)
+			}
+
+			resizeCanvas  = function() {
+				// Set canvas width and height to match the window's size or a percentage of it
+				canvas.width = window.innerWidth * 0.6; // 90% of window width
+				canvas.height = window.innerHeight * 0.5; // 80% of window height
+			
+				// Optional: Redraw any content or background after resizing
+				ctx.fillStyle = 'black';
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
 			}
 		}
 		const board = new Board(canvas.width / 2 ,canvas.height / 2, canvas.width, canvas.height);
@@ -54,9 +68,6 @@ export default function pong() {
 		const ball = new Ball(canvas.width / 2 ,canvas.height / 2);
 		const player1 = new Player("user1");
 		const player2 = new Player("user2");
-		// const player1 = new Player(paddle_distance_from_border, 280, 10, 65);
-		// const player2 = new Player(canvas.width - paddle_distance_from_border - 10, 280, 10, 65);
-
 		ctx.font = "50px Arial";
 		ctx.fillText("Pong", 550, 80);
     }, 0);
